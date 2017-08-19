5850d00935d39996f92cd05b46141ad2f42a1ebd
Sanchez <adrian.sanchez@emerson.com>
2017-08-19 15:49:38 -0500

    Initial webpack compiler implementation.

---
+++ src/client/index.tsx
@@ -0,0 +1,5 @@
+import Temp from "@client/temp";
+
+const temp = new Temp();
+console.log(temp.foo());
+console.log(Object.values(temp));
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require("path");
+
+// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
+const { CheckerPlugin } = require('awesome-typescript-loader');
+
+module.exports = {
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+
+        /**
+         * NOTE:  TsConfigPathsPlugin doesn't seem to be working.  So we need to add these so that webpack
+         * can resolve path aliases.
+        */
+        alias: {
+			"@client": path.resolve(__dirname, "src/client/"),
+			"@server": path.resolve(__dirname, "src/server/"),
+		}
+    },
+
+    entry: [
+        "./src/client/index"
+    ],
+
+    devtool: 'source-map',
+
+    module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader'
+        }]
+    },
+
+    plugins: [
+        new CheckerPlugin()
+    ],
+
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+};
